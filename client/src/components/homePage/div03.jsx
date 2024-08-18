3a217fb276591cefec8614f15410b8cf75f5994a
Madhawa Rathnayake <madhawasoftnet@gmail.com>
2024-08-18 11:19:22 +0530

    getting changes

---
+++ client/src/components/homePage/div03.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Div03 = () => {
+  return (
+    <section className="bg-blue-100 mt-12 p-0">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-3 p-4 max-w-[400px] md:max-w-[600px] place-items-center">
+          <img
+            className="hover:opacity-75"
+            src="https://source.unsplash.com/collection/1346951/150x150?sig=2"
+          />
+          <img
+            className="hover:opacity-75"
+            src="https://source.unsplash.com/collection/1346951/150x150?sig=3"
+          />
+          <img
+            className="hover:opacity-75"
+            src="https://source.unsplash.com/collection/1346951/150x150?sig=4"
+          />
+          <img
+            className="hover:opacity-75"
+            src="https://source.unsplash.com/collection/1346951/150x150?sig=5"
+          />
+          <img
+            className="hover:opacity-75"
+            src="https://source.unsplash.com/collection/1346951/150x150?sig=6"
+          />
+          <img
+            className="hover:opacity-75"
+            src="https://source.unsplash.com/collection/1346951/150x150?sig=1"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Div03;
