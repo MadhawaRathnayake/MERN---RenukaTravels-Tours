fca72806b895ed87330300a386f4a52b4303a811
PiyumalKK <piyumalm912@gmail.com>
2024-09-26 18:41:10 +0530

    Google Auth is done for both sign in and signout

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import {Alert, Button, Label, TextInput, Spinner } from "flowbite-react";
 import { Link, useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
-import { AiFillGoogleCircle } from "react-icons/ai";
 import signupImg from "../images/Signup.png";
 import signupImg2 from "../images/signip1.png";
+import OAuth from "../components/OAuth";
 
 export default function Register() {
   const [formData, setFormData] = useState({});
@@ -154,15 +154,7 @@ export default function Register() {
                 "Sign Up"
               )}
             </Button>
-            <Button
-              type="button"
-              gradientDuoTone="purpleToPink"
-              outline
-              onClick={handleGoogleClick}
-            >
-              <AiFillGoogleCircle className="w-6 h-6 mr-2 text-blue-500" />
-              Continue With Google
-            </Button>
+            <OAuth />
           </form>
           <div className="flex gap-2 text-sm mt-5 text-gray-600 dark:text-gray-300">
             <span>Have an account already?</span>
