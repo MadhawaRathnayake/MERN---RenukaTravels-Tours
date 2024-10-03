49e9fe8e00c4a5647e9a22d97155874b8228529f
Madhawa Rathnayake <madhawasoftnet@gmail.com>
2024-10-03 23:23:53 +0530

    Resolving Map Page errors

--- client/src/pages/MapPage.jsx
+++ client/src/pages/MapPage.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-import CustomMap from "../components/mapPage/Map";
-import MapSelectionBox from "../components/mapPage/MapSelectionBox";
-import MapComponent from "../components/mapPage/MapComponent";
 import MapComp from "../components/mapPage/MapComp";
 
 export default function MapPage() {
