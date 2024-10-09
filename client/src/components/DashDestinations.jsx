8d2b69a1f96ce1764fcbe909046f27d83ac4848a
Madhawa Rathnayake <madhawasoftnet@gmail.com>
2024-10-09 18:04:11 +0530

    Adding the correct Styles to the Dashboard Page

--- client/src/components/DashDestinations.jsx
+++ client/src/components/DashDestinations.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function DashDestinations() {
   return (
-    <div className="w-full">
+    <div className="w-full flex justify-end">
       <a href="/create-destination">
         <button className="bg-[#F4AC20] text-white py-2 px-6 rounded-lg hover:bg-[#f49120]">
           Create a new Destination
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,65 +1,58 @@
-
 //import React from 'react'
-import { useEffect, useState } from 'react';
-import {useLocation} from 'react-router-dom';
-import DashSidebar from '../components/DashSidebar';
-import DashProfile from '../components/DashProfile';
-import DashHotels from '../components/DashHotels';
-import DashUsers from '../components/DashUsers';
-import DashboardComp from '../components/DashboardComp';
-import DashDestinations from '../components/DashDestinations';
-import DashTours from '../components/DashTours';
-import DashVehicles from '../components/DashVehicles';
-import DashBookings from '../components/DashBookings';
-import DashSubscribers from '../components/DashSubscribers';
-
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
+import DashHotels from "../components/DashHotels";
+import DashUsers from "../components/DashUsers";
+import DashboardComp from "../components/DashboardComp";
+import DashDestinations from "../components/DashDestinations";
+import DashTours from "../components/DashTours";
+import DashVehicles from "../components/DashVehicles";
+import DashBookings from "../components/DashBookings";
+import DashSubscribers from "../components/DashSubscribers";
 
 function Dashboard() {
-
-const location = useLocation();
-// eslint-disable-next-line no-unused-vars
-const [tab,setTab]=useState('');
-useEffect(()=>{
-const urlParams = new URLSearchParams(location.search);
-const tabFromUrl = urlParams.get('tab');
-//console.log(tabFromUrl);
-if(tabFromUrl)
-  {
-    setTab(tabFromUrl);
-  }
-
-}, [location.search]);
+  const location = useLocation();
+  // eslint-disable-next-line no-unused-vars
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    //console.log(tabFromUrl);
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
 
   return (
-    <div className="min-h-screen flex flex-col md:flex-row">
-       <div className='lg:ml-32 md:w-56'>
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-row lg:flex-row lg:items-center md:items-center sm:items-center items-center">
+      <div className="md:w-56">
         {/*Sidebar*/}
-        <DashSidebar/>
-       </div>
+        <DashSidebar />
+      </div>
       {/*Profile*/}
-      {tab==='profile' && <DashProfile/>}
+      {tab === "profile" && <DashProfile />}
       {/*Posts*/}
-      {tab==='hotels' && <DashHotels/>}
+      {tab === "hotels" && <DashHotels />}
       {/*Posts*/}
-      {tab==='users' && <DashUsers/>}
-     
+      {tab === "users" && <DashUsers />}
+
       {/*Dashboard*/}
-      {tab==='dash' && <DashboardComp/>}
+      {tab === "dash" && <DashboardComp />}
       {/*Destinations*/}
-      {tab==='destinations' && <DashDestinations/>}
+      {tab === "destinations" && <DashDestinations />}
       {/*Tours*/}
-      
-      {tab==='tours' && <DashTours/>}
-        {/*Vehicless*/}
-        {tab==='vehicles' && <DashVehicles/>}
+
+      {tab === "tours" && <DashTours />}
+      {/*Vehicless*/}
+      {tab === "vehicles" && <DashVehicles />}
       {/*Bookings*/}
-      {tab==='bookings' && <DashBookings/>}
+      {tab === "bookings" && <DashBookings />}
       {/*Subscribers*/}
-      {tab==='subscribers' && <DashSubscribers/>}
-
-
+      {tab === "subscribers" && <DashSubscribers />}
     </div>
-  )
+  );
 }
 
 export default Dashboard;
