3d21a21df9004435f6c8b2e6c6d061a50c5262df
PiyumalKK <piyumalm912@gmail.com>
2024-10-29 09:23:56 +0530

    A

--- client/src/components/mapPage/MapComp.jsx
+++ client/src/components/mapPage/MapComp.jsx
@@ -9,6 +9,7 @@ import {
 } from "@vis.gl/react-google-maps";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import { Datepicker, TextInput, Timeline, Button } from "flowbite-react";
+import VehicleSection from "./VehicleSection";
 
 export default function MapComp() {
   const [formData, setFormData] = useState({});
@@ -328,7 +329,7 @@ export default function MapComp() {
           </div>
         </div>
         <div id="Third-Row-Timeline-Activities" className="">Hotel Details</div>
-        <div id="Third-Row-Timeline-Activities" className="">Vehicle Details</div>
+        <div id="Third-Row-Timeline-Activities" className=""><VehicleSection/></div>
       </form>
     </section>
   );
--- client/src/pages/MapPage.jsx
+++ client/src/pages/MapPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MapComp from "../components/mapPage/MapComp";
 
+
 export default function MapPage() {
   return (
     <div className="max-w-7xl mx-auto">
       <MapComp />
+     
     </div>
   );
 }
