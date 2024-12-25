4f782b2120675e20e331269dcc030e9ebdc83928
Madhawa Rathnayake <madhawasoftnet@gmail.com>
2024-12-26 00:18:36 +0530

    Updating the changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,11 @@ import Dashboard from "./pages/Dashboard";
 import SignIn from "./pages/SignIn";
 import About from "./pages/About";
 import MapPage from "./pages/MapPage";
-import HomePage from "./pages/HomePage";
 import Register from "./pages/Register";
 import DestinationDetails from "./pages/DestinationDetails";
 import Hotels from "./pages/Hotels";
 import Vehicles from "./pages/Vehicles";
 import Tours from "./pages/Tours";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
@@ -27,48 +25,47 @@ import NavigationBar from "./components/NavigationBar";
 export default function App() {
   return (
     <div className="body-color">
-    <BrowserRouter>
-      <NavigationBar />
-      <Routes>
-        <Route path="/" element={<NewHomePage />}></Route>
-        <Route path="/Home" element={<HomePage />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/gallery" element={<Gallery />}></Route>
-        <Route path="/tours" element={<Tours />}></Route>
-        <Route
-          path="/destinations/:destSlug"
-          element={<DestinationDetails />}
-        ></Route>
-        <Route element={<PrivateRoute />}>
-          <Route path="/dashboard" element={<Dashboard />}></Route>
-          <Route path="/map" element={<MapPage />}></Route>
-          <Route path="/mappage" element={<MapPage />}></Route>
-        </Route>
-        <Route element={<OnlyAdminPrivateRoute />}>
+      <BrowserRouter>
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<NewHomePage />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/gallery" element={<Gallery />}></Route>
+          <Route path="/tours" element={<Tours />}></Route>
           <Route
-            path="/create-destination"
-            element={<CreateDestination />}
+            path="/destinations/:destSlug"
+            element={<DestinationDetails />}
           ></Route>
-          <Route
-            path="/update-destination/:destId"
-            element={<UpdateDestination />}
-          ></Route>
-          <Route
-            path="/update-vehicle/:vehicleId"
-            element={<UpdateVehicle />}
-          ></Route>
-        </Route>
-        <Route path="/signin" element={<SignIn />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-        <Route path="/hotels" element={<Hotels />}></Route>
-        <Route path="/vehicles" element={<Vehicles />}></Route>
-        <Route path="/tours" element={<Tours />}></Route>
-        <Route path="/hotels/:id" element={<HotelDetails />} />
-        <Route path="/add-hotel" element={<InputHotel />} />
-        <Route path="/contact" element={<ContactUs />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+          <Route element={<PrivateRoute />}>
+            <Route path="/dashboard" element={<Dashboard />}></Route>
+            <Route path="/map" element={<MapPage />}></Route>
+            <Route path="/mappage" element={<MapPage />}></Route>
+          </Route>
+          <Route element={<OnlyAdminPrivateRoute />}>
+            <Route
+              path="/create-destination"
+              element={<CreateDestination />}
+            ></Route>
+            <Route
+              path="/update-destination/:destId"
+              element={<UpdateDestination />}
+            ></Route>
+            <Route
+              path="/update-vehicle/:vehicleId"
+              element={<UpdateVehicle />}
+            ></Route>
+          </Route>
+          <Route path="/signin" element={<SignIn />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/hotels" element={<Hotels />}></Route>
+          <Route path="/vehicles" element={<Vehicles />}></Route>
+          <Route path="/tours" element={<Tours />}></Route>
+          <Route path="/hotels/:id" element={<HotelDetails />} />
+          <Route path="/add-hotel" element={<InputHotel />} />
+          <Route path="/contact" element={<ContactUs />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
