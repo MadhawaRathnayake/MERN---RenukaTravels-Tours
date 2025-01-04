9cba5a5cd4219771bac8b85c69df3639afa8cf99
oshadakavinda <oshadakavinda2@gmail.com>
2025-01-05 04:13:02 +0530

    ..

--- client/src/components/homePage/section04.jsx
+++ client/src/components/homePage/section04.jsx
@@ -1,15 +1,15 @@
-import { Container, Row, Col } from 'reactstrap';
-import Subtitle from '../shared/Subtitle';
-import './section3.css';
-import FeaturedTourList from '../homePage/FeaturedTourList';
+import { Container, Row, Col } from "reactstrap";
+import Subtitle from "../shared/Subtitle";
+import "./section4.css";
+import FeaturedTourList from "./FeaturedTourList";
 
-export default function Section03() {
+export default function Section04() {
   return (
     <section>
       <Container>
         <Row>
           <Col lg="12" className="mb-5">
-            <Subtitle subtitle={'Explore'} />
+            <Subtitle subtitle={"Explore"} />
             <h2 className="featured__tour-title">Our featured tours</h2>
           </Col>
         </Row>
